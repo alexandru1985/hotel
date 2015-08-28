12786f58263fc3331878590ec34e57be1da8eb5f
alexandru1985 <alexandru_panzaru@yahoo.com>
2015-08-28 22:05:41 +0300

    proiect

--- insert_data1.php
+++ insert_data1.php
@@ -1,12 +1,14 @@
 <?php
 
-$motivul_contactarii = $_POST['motivul_contactarii'];
-$nume = $_POST['nume'];
-$prenume = $_POST['prenume'];
-$mail = $_POST['mail'];
-$telefon = $_POST['telefon'];
-$subiect_mesaj = $_POST['subiect_mesaj'];
-$mesaj = $_POST['mesaj'];
+if (isset($_POST['sub'])) {
+    $client = $_POST['client'];
+    $telefon = $_POST['telefon'];
+    $mail = $_POST['mail'];
+    $numar_zile = $_POST['numar_zile'];
+    $data = $_POST['data'];
+    $tipul_camerei = $_POST['tipul_camerei'];
+    $mesaj = $_POST['mesaj'];
+}
 
 $connect = new mysqli('localhost', 'root', '', 'hotel');
 
@@ -16,21 +18,20 @@ if (mysqli_connect_errno()) {
 }
 
 // interogare sql cu CREATE DATABASE
-$sql = "INSERT INTO clients (MOTIVUL_CONTACTARII,NUME, PRENUME,	MAIL, TELEFON, SUBIECT, MESAJ) VALUES"
-    . " ('$motivul_contactarii','$nume',"
-    . "'$prenume','$mail','$telefon','$subiect_mesaj','$mesaj')";
-
-
-if (empty($_POST['nume']) || empty($_POST['prenume']) || empty($_POST['mail']) || empty($_POST['telefon']) || empty($_POST['subiect_mesaj']) || empty($_POST['mesaj'])) {
-    echo '<div id = mesaj1>';
-    echo "Mesajul nu a fost trimis! Pentru a putea trimite mesajul trebuie sa completati toate campurile!";
+$sql = "INSERT INTO comenzi (CLIENT, TELEFON, MAIL, NUMAR_ZILE, DATA, TIPUL_CAMEREI, MESAJ) VALUES"
+    . " ('$client', '$telefon','$mail','$numar_zile','$data','$tipul_camerei','$mesaj')";
+if (empty($_POST['client']) || empty($_POST['telefon']) || empty($_POST['mail']) || empty($_POST['numar_zile']) ||
+    empty($_POST['data']) || empty($_POST['tipul_camerei']) || empty($_POST['mesaj'])) {
+    echo '<div id=mesaj2>';
+    echo "Rezervarea nu a fost trimisa! Pentru a putea trimite rezervarea trebuie sa completati toate campurile!";
     echo '</div>';
 }
-// executa interogarea $sql pe server pentru a crea baza de date   
+// executa interogarea $sql pe server pentru a crea baza de date
 elseif ($connect->query($sql) === TRUE) {
-    echo '<div id = mesaj>';
-    echo 'Mesajul a fost trimis!';
+    echo '<div id=mesaj>';
+    echo 'Rezervarea a fost trimisa!';
     echo '</div>';
 }
+
 $connect->close();
 ?>
